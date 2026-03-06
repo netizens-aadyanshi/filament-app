7012541bdf082f387d1307f545060b71d01844c7
netizens-aadyanshi <aadyanshi.p@netizens.email>
2026-03-06 11:28:55 +0530

    task3 columns

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -9,6 +9,9 @@ use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn\json_decode;
+use Filament\Support\Enums\FontWeight;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\IconColumn;
 
 class UsersTable
 {
@@ -17,14 +20,27 @@ class UsersTable
         return $table
             ->columns([
                 //
-                ImageColumn::make('profile_photo')->label('Profile Photo')->circular(),
-                TextColumn::make('name')->sortable(),
-                TextColumn::make('email')->sortable(),
+                ImageColumn::make('profile_photo')->label('Profile Photo')->circular()->imageHeight(40)
+                ->defaultImageUrl(function ($record) {
+                        return 'https://ui-avatars.com/api/?background=random&name=' . urlencode($record->name);
+                    }),
+                TextColumn::make('name')->sortable()->searchable()->copyable()->copyMessage('Name copied!!')->weight(FontWeight::SemiBold),
+                TextColumn::make('email')->sortable()->searchable()->copyable()->icon(Heroicon::Envelope),
                 TextColumn::make('phone_number')->label('Phone Number')->sortable(),
-                TextColumn::make('role'),
-                TextColumn::make('email_verified_at')->label('Email Verified'),
+                TextColumn::make('role')->badge(),
+                IconColumn::make('email_verified_at')
+                    ->label('Verified')
+
+                    ->getStateUsing(fn ($record): bool => filled($record->email_verified_at))
+                    ->boolean()
+                    ->trueIcon('heroicon-o-check-badge')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->trueColor('success') // Green
+                    ->falseColor('danger')  // Red
+                    ->alignCenter(),
+                TextColumn::make('created_at')->label('Registered')->dateTime('d M Y')->sortable()->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('interests'),
-                TextColumn::make('status'),
+                TextColumn::make('status')->badge(),
                 TextColumn::make('bio'),
                 TextColumn::make('notes')->label('Internal Notes'),
                 TextColumn::make('address')->label('Saved Addresses'),
